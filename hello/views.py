437420bd35318d801692aca62f81412ebb1b9c44
eiichi-mamada <eastrage@gmail.com>
2020-04-06 16:03:09 +0900

    データベースの表示

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
+from django.db.models import QuerySet
+
+def __new_str__(self):
+	result = ''
+	for item in self:
+		result += '<tr>'
+		for k in item:
+			result += '<td>' + str(k) + '=' + str(item[k]) +'</td>'
+		result += '</tr>'
+	return result
+
+QuerySet.__str__ = __new_str__
 
 def index(request):
-	data = Friend.objects.all()
+	data = Friend.objects.all().values('id', 'name', 'age')
 	params = {
 		'title': 'Hello',
-		'message': 'all friends.',
 		'data': data,
-	}
-	return render(request, 'hello/index.html',params)
+		}
+	return render(request, 'hello/index.html', params)
