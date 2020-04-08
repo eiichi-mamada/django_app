2f1abffbcb2d22ff7ae66dbe48e9a5c89aeaaf9e
eiichi-mamada <eastrage@gmail.com>
2020-04-08 16:27:58 +0900

    ミニSNS,urlパターンの追記まで。

--- hello/forms.py
+++ hello/forms.py
@@ -3,7 +3,6 @@ from django import forms
 class HelloForm(forms.Form):
   name = forms.CharField(label='Name')
   mail = forms.EmailField(label='Email')
-  gender = forms.BooleanField(label='Gender',require=False)
+  gender = forms.BooleanField(label='Gender',required=False)
   age = forms.IntegerField(label='Age')
   birthday = forms.DateField(label='Birth')
-  
